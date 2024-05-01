059c183177f8fd3ac798cd6bdd343839bf911a0f
MariaNanys <maria.iwaszczenko@gmail.com>
2024-05-01 18:04:50 +0200

    Make solutions to first and second tasks.

---
+++ homeworks/06-js-basics-1/script.js
@@ -0,0 +1,34 @@
+//  task 1 https://www.codewars.com/kata/opposite-number
+
+function opposite(number) {
+  return -number;
+}
+
+//  task 2 https://www.codewars.com/kata/basic-mathematical-operations
+
+function basicOp(operation, value1, value2) {
+  let result;
+  switch (operation) {
+    case "+":
+      result = value1 + value2;
+      `${value1} + ${value2} = ${result}`;
+      return result;
+    case "-":
+      result = value1 - value2;
+      `${value1} - ${value2} = ${result}`;
+      return result;
+    case "*":
+      result = value1 * value2;
+      `${value1} * ${value2} = ${result}`;
+      return result;
+    case "/":
+      result = value1 / value2;
+      `${value1} / ${value2} = ${result}`;
+      return result;
+    default:
+      return 0;
+  }
+}
+
+//  task 3 https://www.codewars.com/kata/printing-array-elements-with-comma-delimiters
+
